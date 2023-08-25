c81fadaefecac6d69d55915a79ed4f80c7f98d85
xyhtac <73641145+xyhtac@users.noreply.github.com>
2023-08-25 17:01:38 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,8 +3,8 @@
 FILE=/opt/tg2sip/db.sqlite
 if [ -f "$FILE" ]; then
     echo "$FILE exists. Staring TG Client."
-    sh /opt/tg2sip/tg2sip
+    sh "tg2sip"
 else 
     echo "$FILE does not exist. Running GEN_DB."
-    sh /opt/tg2sip/gen_db
+    sh "gen_db"
 fi
