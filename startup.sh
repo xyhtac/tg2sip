0c8f72a833f6c1f4c2195d7afdf5b47f3033cf10
xyhtac <73641145+xyhtac@users.noreply.github.com>
2023-08-25 18:18:11 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,6 +8,6 @@ if [ -f "$FILE" ]; then
     echo "$FILE exists. Starting TG Client."
     /opt/tg2sip/tg2sip
 else 
-    echo "$FILE does not exist. Running user prompt."
-    /bin/bash
+    echo "$FILE does not exist. Running user ping."
+    ping 8.8.8.8
 fi
